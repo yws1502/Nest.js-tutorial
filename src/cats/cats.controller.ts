727e9bf7760b3543518014aecd0f2228e238bb7c
Woosang <yws1502@naver.com>
2022-10-09 18:24:21 +0900

    ✨ 회원가입 api 기능 추가

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,5 @@
 import {
+  Body,
   Controller,
   Get,
   Post,
@@ -8,6 +9,7 @@ import {
 import { HttpExceptionFilter } from 'src/common/exceptions/http-exception.filter';
 import { CatsService } from './cats.service';
 import { SuccessInterceptor } from 'src/common/interceptors/success.interceptor';
+import { CatRequestDto } from './dto/cats.request.dto';
 
 @Controller('cats')
 @UseInterceptors(SuccessInterceptor)
@@ -22,8 +24,8 @@ export class CatsController {
   }
 
   @Post()
-  async signUp() {
-    return 'signup';
+  async signUp(@Body() body: CatRequestDto) {
+    return await this.catsService.signUp(body);
   }
 
   @Post('login')
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcrypt';
+import { Cat } from './cats.schema';
+import { CatRequestDto } from './dto/cats.request.dto';
 
 @Injectable()
-export class CatsService {}
+export class CatsService {
+  constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
+
+  async signUp(body: CatRequestDto) {
+    const { email, name, password } = body;
+    const isCatExist = await this.catModel.exists({ email });
+
+    // 존재 여부 확인
+    if (isCatExist) {
+      throw new UnauthorizedException('해당하는 고양이는 이미 존재합니다.');
+    }
+
+    // password 암호화
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // 계정 생성 로직
+    const cat = await this.catModel.create({
+      email,
+      name,
+      password: hashedPassword,
+    });
+
+    return cat.readOnlyData;
+  }
+}
